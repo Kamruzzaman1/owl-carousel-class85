14a723b042aaf734b0a0919ccbc78a37ae2c7e54
Kamruzzaman <75533785+Kamruzzaman1@users.noreply.github.com>
2021-10-06 18:40:12 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+(function($){
+	"use strict";
+$(document).ready(function(){
+
+    $('.testmonial-slide').owlCarousel({
+        loop:true,
+        items:1,
+        margin:10,
+        nav:true,
+        navText:["<i class='fas fa-long-arrow-alt-left'></i>", "<i class='fas fa-long-arrow-alt-right'></i>"],
+        autoplay:4000,
+        autoplayTimeout:4000,
+        dots:false,
+
+
+
+});
+    })
+
+
+   jQuery(window).load(function(){
+
+   	
+});
+
+}(jQuery));
